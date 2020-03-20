8ee3e7ecaebb43d8e93ee4c78c1a1626ef8fbd65
Googler <noreply@google.com>
2020-03-20 14:43:45 +0100

    Fix test flakyness. Tested with: blaze test --runs_per_test=2000 //third_party/dart/intl_translation/...
    PiperOrigin-RevId: 302017241

--- test/message_extraction/message_extraction_test.dart
+++ test/message_extraction/message_extraction_test.dart
@@ -2,7 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@Timeout(const Duration(seconds: 60))
+@Timeout(const Duration(seconds: 180))
+
 library message_extraction_test;
 
 import 'package:test/test.dart';
