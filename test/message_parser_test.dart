2d762cade2e710c37574b47e57b160cc0933f0f0
moritz <mosum@google.com>
2022-09-21 11:42:28 +0000

    Add copyright statement

--- test/message_parser_test.dart
+++ test/message_parser_test.dart
@@ -1,4 +1,8 @@
-// @dart=2.10
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:intl_translation/src/intl_message.dart';
 import 'package:intl_translation/src/message_parser.dart';
 import 'package:test/test.dart';
