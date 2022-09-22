0c5e0de39ff658854c37b3c451fc206384aaf874
moritz <mosum@google.com>
2022-09-22 12:59:32 +0000

    add licenses

--- lib/src/messages/submessages/plural.dart
+++ lib/src/messages/submessages/plural.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:intl_translation/src/messages/message.dart';
 import 'package:intl_translation/src/messages/submessages/submessage.dart';
 
