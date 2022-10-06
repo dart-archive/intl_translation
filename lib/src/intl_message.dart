eed65efc9345b4e83809fc42b911089f0cc40646
moritz <mosum@google.com>
2022-10-06 13:28:54 +0000

    Merge with master

--- lib/src/intl_message.dart
+++ lib/src/intl_message.dart
@@ -4,9 +4,6 @@
 
 // @dart=2.10
 
-// ignore_for_file: deprecated_member_use, #168
-// ignore_for_file: implementation_imports
-
 /// This provides classes to represent the internal structure of the
 /// arguments to `Intl.message`. It is used when parsing sources to extract
 /// messages or to generate code for message substitution. Normal programs
@@ -356,7 +353,7 @@ class CompositeMessage extends Message {
   List<Message> pieces;
 
   CompositeMessage.withParent(parent) : super(parent);
-  CompositeMessage(this.pieces, ComplexMessage parent) : super(parent) {
+  CompositeMessage(this.pieces, [ComplexMessage parent]) : super(parent) {
     for (var x in pieces) {
       x.parent = this;
     }
@@ -372,10 +369,28 @@ class CompositeMessage extends Message {
       pieces.map((chunk) => transform(this, chunk)).join('');
 }
 
+/// A pair of two messages, similar to [CompositeMessage].
+class PairMessage<T extends Message, S extends Message> extends Message {
+  final T first;
+  final S second;
+
+  PairMessage(this.first, this.second, [Message parent]) : super(parent);
+
+  @override
+  String expanded([Function transform = _nullTransform]) =>
+      [first, second].map((chunk) => transform(this, chunk)).join('');
+
+  @override
+  String toCode() => [first, second].map((each) => each.toCode()).join('');
+
+  @override
+  Object toJson() => [first, second].map((each) => each.toJson()).toList();
+}
+
 /// Represents a simple constant string with no dynamic elements.
 class LiteralString extends Message {
   String string;
-  LiteralString(this.string, Message parent) : super(parent);
+  LiteralString(this.string, [Message parent]) : super(parent);
   @override
   String toCode() => escapeAndValidateString(string);
   @override
@@ -398,7 +413,8 @@ class VariableSubstitution extends Message {
   /// may have been used as all upper-case in the translation tool, so we
   /// save it separately and look it up case-insensitively once the parent
   /// (and its arguments) are definitely available.
-  VariableSubstitution.named(String name, Message parent) : super(parent) {
+  VariableSubstitution.named(String name, [Message parent]) : super(parent) {
+    _variableName = name;
     _variableNameUpper = name.toUpperCase();
   }
 
@@ -428,8 +444,12 @@ class VariableSubstitution extends Message {
 
   /// The name of the variable in the parameter list of the containing function.
   /// Used when generating code for the interpolation.
-  String get variableName =>
-      _variableName ?? (_variableName = arguments[index]);
+  String get variableName {
+    return arguments != null && index != null
+        ? arguments[index]
+        : _variableName;
+  }
+
   String _variableName;
   // Although we only allow simple variable references, we always enclose them
   // in curly braces so that there's no possibility of ambiguity with
@@ -439,7 +459,7 @@ class VariableSubstitution extends Message {
   @override
   int toJson() => index;
   @override
-  String toString() => 'VariableSubstitution($index)';
+  String toString() => 'VariableSubstitution(${index ?? _variableName})';
   @override
   String expanded([Function transform = _nullTransform]) =>
       transform(this, index);
@@ -697,7 +717,22 @@ abstract class SubMessage extends ComplexMessage {
   /// as a list of [key, value] where value may in turn be a list.
   SubMessage.from(this.mainArgument, List clauses, parent) : super(parent) {
     for (var clause in clauses) {
-      this[clause.first] = (clause.last is List) ? clause.last : [clause.last];
+      String key;
+      Object value;
+      if (clause is List && clause[0] is String && clause.length == 2) {
+        // If trying to parse a string
+        key = clause[0];
+        value = (clause[1] is List) ? clause[1] : [(clause[1])];
+      } else if (clause is PairMessage<LiteralString, Message>) {
+        // If trying to parse a message
+        key = clause.first.string;
+        Message second = clause.second;
+        value = second is CompositeMessage ? second.pieces : [second];
+      } else {
+        throw Exception(
+            'The clauses argument supplied must be a list of pairs, i.e. list of lists of length 2 or PairMessages.');
+      }
+      this[key] = value;
     }
   }
 
